52564ba6f2da97c7f5c79d6d43627ab18eadc66b
Jesse-Lee Weeder <ST10320806@vcconnect.edu.za>
2024-08-30 18:27:55 +0200

    Add project files.

---
+++ Services/QueueService.cs
@@ -0,0 +1,22 @@
+﻿using Azure.Storage.Queues;
+
+namespace ST10320806_Part1.Services
+{
+    public class QueueService
+    { 
+
+        private readonly QueueServiceClient _queueServiceClient;
+
+    public QueueService(IConfiguration configuration)
+    {
+        _queueServiceClient = new QueueServiceClient(configuration["AzureStorage:ConnectionString"]);
+    }
+
+    public async Task SendMessageAsync(string queueName, string message)
+    {
+        var queueClient = _queueServiceClient.GetQueueClient(queueName);
+        await queueClient.CreateIfNotExistsAsync();
+        await queueClient.SendMessageAsync(message);
+    }
+}
+}
---
+++ Services/TableService.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Data.Tables;
+using ST10320806_Part1.Models;
+
+namespace ST10320806_Part1.Services
+{
+    public class TableService
+    {
+        private readonly TableClient _tableClient;
+
+        public TableService(IConfiguration configuration)
+        {
+            var connectionString = configuration["AzureStorage:ConnectionString"];
+            var serviceClient = new TableServiceClient(connectionString);
+            _tableClient = serviceClient.GetTableClient("CustomerProfiles");
+            _tableClient.CreateIfNotExists();
+        }
+
+        public async Task AddEntityAsync(CustomerProfile profile)
+        {
+            await _tableClient.AddEntityAsync(profile);
+        }
+    }
+}
